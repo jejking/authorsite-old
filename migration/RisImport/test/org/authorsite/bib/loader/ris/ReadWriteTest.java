9e6e263e0874529ecae1bc22a3c827c6be81d5ab
jejking <jejking@ba34ae75-9d15-0410-b136-fd2a35c96f86>
2006-07-10 16:19:19 +0000

    Simple class to write out the SQL produced.

---
+++ migration/RisImport/test/org/authorsite/bib/loader/ris/ReadWriteTest.java
@@ -0,0 +1,32 @@
+package org.authorsite.bib.loader.ris;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import junit.framework.TestCase;
+
+
+public class ReadWriteTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        File f = new java.io.File("biblio.sql");
+        if ( f.exists() ) {
+            f.delete();
+        }
+    }
+
+    public void testReadWrite() throws Exception {
+        Parser p = new Parser();
+        p.readFile("biblio.txt"); 
+        Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream("biblio.sql"), "UTF8"));
+            
+        Bibliography.getInstance().writeBibliographyToSql(writer);
+        writer.close();
+    }
+    
+}
