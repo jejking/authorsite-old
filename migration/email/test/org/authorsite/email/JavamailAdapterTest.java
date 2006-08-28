aaf0b17af55a7629bdd2153a5148b4c1ad3e7782
jejking <jejking@ba34ae75-9d15-0410-b136-fd2a35c96f86>
2006-08-28 21:13:26 +0000

    Started on some test functionality.

---
+++ migration/email/test/org/authorsite/email/JavamailAdapterTest.java
@@ -0,0 +1,59 @@
+package org.authorsite.email;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Folder;
+import javax.mail.Session;
+import javax.mail.Store;
+import javax.mail.URLName;
+import javax.mail.internet.InternetAddress;
+
+import junit.framework.TestCase;
+
+public class JavamailAdapterTest extends TestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testConvertFolder() throws Exception {
+        
+        Session session = Session.getDefaultInstance(new Properties());
+
+        Store store = session.getStore(new URLName("mstor:email/mboxes/oldTest1"));
+        store.connect();
+
+        // read messages from Inbox..
+        Folder inbox = store.getDefaultFolder();
+        inbox.open(Folder.READ_ONLY);
+
+        JavamailAdapter a = new JavamailAdapter();
+        a.convertFolder(inbox);
+        
+        store.close();
+    }
+
+    public void testConvertMessage() {
+        fail("Not yet implemented");
+    }
+
+    public void testConvertEmailAddress() throws Exception {
+        InternetAddress one = new InternetAddress();
+        one.setAddress("test@test.com");
+        one.setPersonal("Test Test");
+        
+        EmailAddressing oneA = new JavamailAdapter().convertEmailAddress(one);
+        assertEquals(one.getAddress(), oneA.getEmailAddress());
+        assertEquals(one.getPersonal(), oneA.getPersonalName());
+    }
+
+    public void testConvertMultipart() {
+        fail("Not yet implemented");
+    }
+
+}
