1f6411f7b8834811a7f90a40c5b30ded9034e000
jejking <jejking@ba34ae75-9d15-0410-b136-fd2a35c96f86>
2006-08-31 10:48:30 +0000

    further work extending functionality bit by bit

--- migration/email/src/org/authorsite/email/db/MySqlPersisterUtil.java
+++ migration/email/src/org/authorsite/email/db/MySqlPersisterUtil.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.authorsite.email.EmailFolder;
+import org.authorsite.email.EmailMessage;
 
 public class MySqlPersisterUtil {
 
@@ -18,6 +21,12 @@ public class MySqlPersisterUtil {
 	
 	private static final String SELECT_LAST_INSERT_ID = "SELECT LAST_INSERT_ID();";
 	
+	private static final String INSERT_MESSAGE_CORE = 
+		"INSERT INTO parts " +
+		"(created_at, updated_at, type, subject, sentDate, receivedDate, inReplyTo, msgReferences, msgId, textContent, folder_id, folder_position) " +
+		"VALUES " +
+		"(NOW(), NOW(), 'Message', ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+	
 	public void persistFolder(EmailFolder folder, Connection con) throws Exception {
 	
 		assert folder != null;
@@ -27,15 +36,118 @@ public class MySqlPersisterUtil {
 			throw new IllegalStateException("Folder already has db id: " + folder.getId());
 		}
 		
-		// does the folder already exist in the db?
-		if ( findFolder(folder, con) > -1 ) {
-			
+		// does the folder already exist in the db, if not create it and its hierarchy
+		if ( findFolder(folder, con) == -1 ) {
+			createFolderHierarchyInDb(folder, con);
 		}
 		
-		// insert the folder data
+		// persist all its messages
+		int pos = 0;
+		for ( EmailMessage message : folder.getChildMessages() ) {
+			pos++;
+			persistMessage(message, pos, con);
+		}
 		
-		// retrieve its new db id
+		// we do not anticipate child folders at this stage given our input...
 		
+	}
+
+	protected void persistMessage(EmailMessage message, int pos, Connection con) throws SQLException {
+		// persist message skeleton first
+		persistMessageCore(message, pos, con);
+	}
+
+	protected void persistMessageCore(EmailMessage message, int pos, Connection con) throws SQLException {
+		PreparedStatement insertMessageCorePs = con.prepareStatement(MySqlPersisterUtil.INSERT_MESSAGE_CORE);
+		insertMessageCorePs.setString(1, message.getSubject());
+		if( message.getSentDate() != null ) {
+			insertMessageCorePs.setDate(2, new java.sql.Date(message.getSentDate().getTime()));
+		}
+		else {
+			insertMessageCorePs.setDate(2, null);
+		}
+		if ( message.getReceivedDate() != null ) {
+			insertMessageCorePs.setDate(3, new java.sql.Date(message.getReceivedDate().getTime()));
+		}
+		else {
+			insertMessageCorePs.setDate(3, null);
+		}
+		insertMessageCorePs.setString(4, message.getInReplyTo());
+		if ( message.getMsgReferences() != null && !(message.getMsgReferences().isEmpty())) {
+			StringBuilder b = new StringBuilder();
+			for ( String msgRef : message.getMsgReferences() ) {
+				b.append(msgRef);
+				b.append(" ");
+			}
+			insertMessageCorePs.setString(5, b.toString().trim());
+		}
+		else {
+			insertMessageCorePs.setString(5, null);
+		}
+		insertMessageCorePs.setString(6, message.getMsgId());
+		insertMessageCorePs.setString(7, message.getContent());
+		insertMessageCorePs.setLong(8, message.getParent().getId());
+		insertMessageCorePs.setInt(9, pos);
+		
+		insertMessageCorePs.executeUpdate();
+		PreparedStatement getIdPs = con.prepareStatement(MySqlPersisterUtil.SELECT_LAST_INSERT_ID);
+	    ResultSet rs = getIdPs.executeQuery();
+	    rs.next();
+	    long id = rs.getLong(1);
+	    message.setId(id);
+	    rs.close();
+	    
+	    getIdPs.close();
+	    insertMessageCorePs.close();
+	    
+	}
+
+	protected void createFolderHierarchyInDb(EmailFolder folder, Connection con) throws SQLException {
+		// we will put the parents into a list, then check each element exists
+		List<EmailFolder> pathList = convertParentsToList(folder);
+		for (EmailFolder pathFolder : pathList ) {
+			long folderIdInDB = findFolder(pathFolder, con);
+			if ( folderIdInDB == -1) {
+				pathFolder.setId(createSingleFolderInDb(pathFolder, con));
+			}
+			else {
+				pathFolder.setId( folderIdInDB);
+			}
+		}
+	}
+	
+	
+
+	protected long createSingleFolderInDb(EmailFolder pathFolder, Connection con) throws SQLException {
+		
+		if ( pathFolder == EmailFolder.ROOT ) {
+			return EmailFolder.ROOT.getId();
+		}
+		
+		PreparedStatement insertFolderPs = con.prepareStatement(MySqlPersisterUtil.INSERT_FOLDER);
+		insertFolderPs.setLong(1, pathFolder.getParent().getId());
+		insertFolderPs.setString(2, pathFolder.getName());
+		insertFolderPs.setString(3, pathFolder.getPath());
+		insertFolderPs.executeUpdate();
+		
+	    PreparedStatement getIdPs = con.prepareStatement(MySqlPersisterUtil.SELECT_LAST_INSERT_ID);
+	    ResultSet rs = getIdPs.executeQuery();
+	    rs.next();
+	    long id = rs.getLong(1);
+	    rs.close();
+		return id;
+	}
+
+	protected List<EmailFolder> convertParentsToList(EmailFolder folder) {
+		ArrayList<EmailFolder> hierarchy = new ArrayList<EmailFolder>();
+		hierarchy.add(folder);
+		EmailFolder parent = folder.getParent();
+		hierarchy.add(0, parent);
+		while (parent.getParent() != null) {
+			parent = parent.getParent();
+			hierarchy.add(0, parent);
+		}
+		return hierarchy;
 	}
 
 	protected long findFolder(EmailFolder folder, Connection con) throws SQLException {
--- migration/email/test/org/authorsite/email/db/MySqlPersisterUtilTest.java
+++ migration/email/test/org/authorsite/email/db/MySqlPersisterUtilTest.java
@@ -3,14 +3,45 @@ package org.authorsite.email.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.authorsite.email.EmailFolder;
+import org.authorsite.email.EmailMessage;
 
 import junit.framework.TestCase;
 
 public class MySqlPersisterUtilTest extends TestCase {
 
 	Connection con;
+	
+	private EmailFolder createABCFolders() {
+		EmailFolder C = new EmailFolder("C");
+		EmailFolder B = new EmailFolder("B");
+		C.setParent(B);
+		EmailFolder A = new EmailFolder("A");
+		B.setParent(A);
+		A.setParent(EmailFolder.ROOT);
+		return C;
+	}
+	
+	private EmailMessage createTestPlainTextMessage() {
+		EmailMessage m = new EmailMessage();
+		m.setParent(createABCFolders());
+		m.setContent("this is the content");
+		m.setMsgId("test1");
+		m.setInReplyTo("test0");
+		m.setSubject("testing is the subject");
+		m.setSentDate(new Date(1157020091283L));
+		m.setReceivedDate(new Date(1157020091283L));
+		List<String> msgReferences = new ArrayList<String>();
+		msgReferences.add("test0");
+		m.setMsgReferences(msgReferences);
+		return m;
+		// TODO addresses...
+	}	
 	
 	@Override
 	protected void setUp() throws Exception {
@@ -38,6 +69,10 @@ public class MySqlPersisterUtilTest extends TestCase {
 		PreparedStatement delFoldersPs = con.prepareStatement("DELETE FROM folders WHERE id > 1;");
 		delFoldersPs.executeUpdate();
 		delFoldersPs.close();
+		
+		PreparedStatement delPartsPs = con.prepareStatement("DELETE FROM parts");
+		delPartsPs.executeUpdate();
+		delFoldersPs.close();
 		con.close();
 	}
 
@@ -48,4 +83,97 @@ public class MySqlPersisterUtilTest extends TestCase {
 		assertTrue(util.findFolder(folderA, con) > 1);
 	}
 	
+	public void testConvertParentsToList() {
+		EmailFolder C = createABCFolders();
+		
+		List<EmailFolder> hierarchyList = new MySqlPersisterUtil().convertParentsToList(C);
+		assertTrue(hierarchyList.size() == 4);
+		assertSame(hierarchyList.get(0), EmailFolder.ROOT);
+		assertEquals("A", hierarchyList.get(1).getName());
+		assertEquals("B", hierarchyList.get(2).getName());
+		assertEquals("C", hierarchyList.get(3).getName());
+	}
+
+	public void testCreateSingleFolderInDb() throws Exception {
+		EmailFolder C = createABCFolders();
+		
+		MySqlPersisterUtil util = new MySqlPersisterUtil();
+		long id = util.createSingleFolderInDb(C, con);
+		
+		assertEquals(id, util.findFolder(C, con));
+	}
+	
+	public void testCreateFolderHierarchyInDb() throws Exception {
+		EmailFolder C = createABCFolders();
+		MySqlPersisterUtil util = new MySqlPersisterUtil();
+		
+		// note, / and A are there already, B + C must be created...
+		
+		util.createFolderHierarchyInDb(C, con);
+		
+		assertTrue(util.findFolder(C, con) > -1);
+		assertTrue(util.findFolder(C.getParent(), con) > -1); 
+	}
+	
+	public void testPersistMessageCore() throws Exception {
+		EmailFolder C = createABCFolders();
+		C.setId(66); // any old number
+		EmailMessage m = createTestPlainTextMessage();
+		m.setParent(C);
+		
+		MySqlPersisterUtil util = new MySqlPersisterUtil();
+		util.persistMessageCore(m, 1, con);
+		
+		long id = m.getId();
+		assertTrue( id > 0 );
+		
+		/*
+		 * id                  INTEGER PRIMARY KEY AUTO_INCREMENT,
+    created_at		    DATETIME,
+    updated_at		    DATETIME,
+    lock_version	    INTEGER NOT NULL DEFAULT 0,
+    type                ENUM (  'Message',
+                                'MimeMultipart',
+                                'MimeBodyPart' ) NOT NULL,
+    subject             VARCHAR(255),
+    sentDate            DATETIME,
+    receivedDate        DATETIME,
+    inReplyTo           VARCHAR(255),
+    msgReferences       VARCHAR (255),
+    msgId               VARCHAR(255),
+    textContent         TEXT,
+    binaryContent       BLOB,
+    mimeType            VARCHAR(255),
+    parent_id           INTEGER NOT NULL REFERENCES parts(id),
+    multipartOrder      INTEGER,
+    folder_id           INTEGER REFERENCES folders(id),
+    folder_position     INTEGER
+		 */
+		PreparedStatement lookup = con.prepareStatement(
+				"SELECT type, subject, sentDate, " +
+				"receivedDate, inReplyTo, msgReferences, " +
+				"msgId, textContent, folder_id, folder_position " +
+				"FROM " +
+				"parts " +
+				"WHERE " +
+				"id = ?");
+		lookup.setLong(1, id);
+		ResultSet rs = lookup.executeQuery();
+		rs.next();
+		assertEquals("Message", rs.getString(1));
+		assertEquals("testing is the subject", rs.getString(2));
+		// dates are a bit iffy at this level..
+//		assertEquals(new java.sql.Date(1157020091283L), rs.getDate(3));
+//		assertEquals(new java.sql.Date(1157020091283L), rs.getDate(4));
+		assertEquals("test0", rs.getString(5));
+		assertEquals("test0", rs.getString(6));
+		assertEquals("test1", rs.getString(7));
+		assertEquals("this is the content", rs.getString(8));
+		assertEquals(66, rs.getLong(9));
+		assertEquals(1, rs.getInt(10));
+		rs.close();
+		lookup.close();
+	}
+
+
 }
