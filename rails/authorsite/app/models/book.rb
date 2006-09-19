8f7334ec9fca81e70c3f4f9b348f663457f1c235
jejking <jejking@ba34ae75-9d15-0410-b136-fd2a35c96f86>
2006-09-19 21:12:23 +0000

    Some small changes, adding more validation.

--- rails/authorsite/app/models/book.rb
+++ rails/authorsite/app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < Work
+  
+  # If the book has had its published flag set to true, checks
+  # that it fulfills the rules for being published:
+  # - defines an author or an editor, that author maybe "Unknown"
+  # - defines a publisher
+  # - defines a from_year field (= year of publication)
+  # - if to_year is defined, it must be *after* from_year
+  def check_for_publication
+    puts "implement publication check"
+  end
 end
