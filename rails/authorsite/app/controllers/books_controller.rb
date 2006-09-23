731dafb073f315d2f278fee2ddd6582d7708a010
jejking <jejking@ba34ae75-9d15-0410-b136-fd2a35c96f86>
2006-09-23 15:07:51 +0000

    starting out with some generated code

---
+++ rails/authorsite/app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  def index
+    list
+    render :action => 'list'
+  end
+
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+         :redirect_to => { :action => :list }
+
+  def list
+    @book_pages, @books = paginate :books, :per_page => 10
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(params[:book])
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(params[:book])
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to :action => 'show', :id => @book
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+    redirect_to :action => 'list'
+  end
+end
