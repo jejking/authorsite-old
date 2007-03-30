d7f194553d5b84bdc30f8b263b4cda8ed656e89a
jejking <jejking@ba34ae75-9d15-0410-b136-fd2a35c96f86>
2007-03-30 15:41:48 +0000

    minor tinkerings

---
+++ maven/web/src/main/java/org/authorsite/web/mvc/controllers/LoginController.java
@@ -0,0 +1,31 @@
+/*
+ * LoginController.java
+ *
+ * Created on 30. März 2007, 10:30
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.authorsite.web.mvc.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+/**
+ *
+ * @author jking
+ */
+public class LoginController extends AbstractController {
+    
+    /** Creates a new instance of LoginController */
+    public LoginController() {
+    }
+
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        return new ModelAndView("auth/login");
+    }
+    
+}
