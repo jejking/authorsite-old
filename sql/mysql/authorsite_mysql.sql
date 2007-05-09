54457ef5f78e187e993933fea2c2480c6ce68f4b
jejking <jejking@ba34ae75-9d15-0410-b136-fd2a35c96f86>
2007-05-09 20:29:47 +0000

    tidying up

--- sql/mysql/authorsite_mysql.sql
+++ sql/mysql/authorsite_mysql.sql
@@ -1,12 +1,20 @@
+DROP TABLE IF EXISTS email_addresses;
+
 DROP TABLE IF EXISTS human_work_relationships;
 
+DROP TABLE IF EXISTS human_work_relationship_types;
+
 DROP TABLE IF EXISTS work_work_relationships;
+
+DROP TABLE IF EXISTS work_work_relationship_types;
 
 DROP TABLE IF EXISTS languages_works;
 
 DROP TABLE IF EXISTS languages;
 
 DROP TABLE IF EXISTS works;
+
+DROP TABLE IF EXISTS work_types;
 
 DROP TABLE IF EXISTS humans;
 
@@ -18,7 +26,9 @@ DROP TABLE IF EXISTS parts;
 
 DROP TABLE IF EXISTS headers;
 
+DROP TABLE IF EXISTS content_folders;
 
+DROP TABLE IF EXISTS content_items;
 
 CREATE TABLE humans (
 
@@ -34,7 +44,7 @@ CREATE TABLE humans (
     given_names         VARCHAR(255),
     name_qualification  VARCHAR(255),
     place               VARCHAR(255),
-    address             TEXT,
+    note                TEXT,
     password            VARCHAR(255)
 
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
@@ -42,27 +52,45 @@ CREATE TABLE humans (
 -- add index on name, given names
 ALTER TABLE humans ADD INDEX HUMAN_NAME_IDX (name(100), given_names(100));
 
+CREATE TABLE email_addresses(
+    id                  INTEGER PRIMARY KEY AUTO_INCREMENT,
+    created_at          DATETIME,
+    updated_at          DATETIME,
+    lock_version        INTEGER NOT NULL DEFAULT 0,
+    address             VARCHAR(255) NOT NULL,
+    human_id            INTEGER NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+ALTER TABLE email_addresses ADD CONSTRAINT ea_h_fk FOREIGN KEY (human_id) REFERENCES humans(id);
+ALTER TABLE email_addresses ADD UNIQUE INDEX ea_uq_idx (address);
+
+CREATE TABLE work_types (
+    type                VARCHAR(255) PRIMARY KEY
+)
+ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+INSERT INTO work_types(type) VALUES ('FrBrWork');
+INSERT INTO work_types(type) VALUES ('Article');
+INSERT INTO work_types(type) VALUES ('Book');
+INSERT INTO work_types(type) VALUES ('Booklet');
+INSERT INTO work_types(type) VALUES ('Conference');
+INSERT INTO work_types(type) VALUES ('Chapter');
+INSERT INTO work_types(type) VALUES ('Inproceedings');
+INSERT INTO work_types(type) VALUES ('Journal');
+INSERT INTO work_types(type) VALUES ('Manual');
+INSERT INTO work_types(type) VALUES ('OnlineResource');
+INSERT INTO work_types(type) VALUES ('Proceedings');
+INSERT INTO work_types(type) VALUES ('Techreport');
+INSERT INTO work_types(type) VALUES ('Thesis');
+INSERT INTO work_types(type) VALUES ('Unpublished');
+
+
 CREATE TABLE works (
     id              INTEGER PRIMARY KEY AUTO_INCREMENT,
     created_at      DATETIME,
     updated_at      DATETIME,
     lock_version    INTEGER NOT NULL DEFAULT 0,
-    type            ENUM(   'FrbrWork',
-                            'Article',
-                            'Book',
-                            'Booklet',
-                            'Conference',
-                            'Chapter',
-                            'Incollection',
-                            'Inproceedings',
-                            'Journal',
-                            'Manual',
-                            'Misc',
-                            'OnlineResource',
-                            'Proceedings',
-                            'Techreport',
-                            'Thesis',
-                            'Unpublished' ) NOT NULL,
+    type            VARCHAR (255) NOT NULL,
     title           TEXT NOT NULL,
     year            YEAR,
     to_year         YEAR,
@@ -80,6 +108,8 @@ CREATE TABLE works (
     published       BOOLEAN NOT NULL DEFAULT 0
 )
 ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+ALTER TABLE works ADD CONSTRAINT w_work_type_fk FOREIGN KEY (type) REFERENCES work_types(type);
 
 CREATE TABLE languages
 (
@@ -102,6 +132,19 @@ ENGINE=InnoDB;
 ALTER TABLE languages_works ADD CONSTRAINT lw_language_fk FOREIGN KEY (language_id)  REFERENCES languages(id);
 ALTER TABLE languages_works ADD CONSTRAINT lw_work_fk FOREIGN KEY (work_id) REFERENCES works(id);
 
+CREATE TABLE human_work_relationship_types (
+    relationship    VARCHAR(255) PRIMARY KEY
+)
+ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+INSERT INTO human_work_relationship_types(relationship) VALUES ('Author');
+INSERT INTO human_work_relationship_types(relationship) VALUES ('Editor');
+INSERT INTO human_work_relationship_types(relationship) VALUES ('Publisher');
+INSERT INTO human_work_relationship_types(relationship) VALUES ('Organization');
+INSERT INTO human_work_relationship_types(relationship) VALUES ('Institution');
+INSERT INTO human_work_relationship_types(relationship) VALUES ('Subject');
+INSERT INTO human_work_relationship_types(relationship) VALUES ('AwardingBody');
+
 CREATE TABLE human_work_relationships
 (
     id                INTEGER PRIMARY KEY AUTO_INCREMENT,
@@ -110,13 +153,7 @@ CREATE TABLE human_work_relationships
     lock_version      INTEGER NOT NULL DEFAULT 0,
     human_id          INTEGER NOT NULL,
     work_id           INTEGER NOT NULL,
-    relationship      ENUM(     'Author',
-                                'Editor',
-                                'Publisher',
-                                'Organization',
-                                'Institution',
-                                'AwardingBody',
-                                'Subject' ) NOT NULL
+    relationship      VARCHAR(255) NOT NULL
 )
 ENGINE=InnoDB CHARACTER SET UTF8 COLLATE utf8_general_ci;
 
@@ -124,6 +161,21 @@ ALTER TABLE human_work_relationships ADD UNIQUE INDEX human2workIdx (human_id, r
 ALTER TABLE human_work_relationships ADD INDEX work2HumanIdx (work_id, relationship, human_id) ;
 ALTER TABLE human_work_relationships ADD CONSTRAINT hwr_human_fk FOREIGN KEY (human_id) REFERENCES humans(id);
 ALTER TABLE human_work_relationships ADD CONSTRAINT hwr_work_fk FOREIGN KEY (work_id) REFERENCES works(id);
+ALTER TABLE human_work_relationships ADD CONSTRAINT hwr_hwr_type_fk FOREIGN KEY (relationship) REFERENCES human_work_relationship_types(relationship);
+
+CREATE TABLE work_work_relationship_types
+(
+    relationship      VARCHAR(255) PRIMARY KEY
+)
+ENGINE=InnoDB CHARACTER SET UTF8 COLLATE utf8_general_ci;
+
+INSERT INTO work_work_relationship_types (relationship) VALUES ('Containment');
+INSERT INTO work_work_relationship_types (relationship) VALUES ('Expression');
+INSERT INTO work_work_relationship_types (relationship) VALUES ('Subject');
+INSERT INTO work_work_relationship_types (relationship) VALUES ('Translation');
+INSERT INTO work_work_relationship_types (relationship) VALUES ('Citation');
+INSERT INTO work_work_relationship_types (relationship) VALUES ('Review');
+
 
 CREATE TABLE work_work_relationships
 (
@@ -133,11 +185,7 @@ CREATE TABLE work_work_relationships
     lock_version      INTEGER NOT NULL DEFAULT 0,
     from_id           INTEGER NOT NULL,
     to_id             INTEGER NOT NULL,
-    relationship      ENUM ('Containment',
-                            'Expression',
-                            'Subject',
-                            'Translation',
-                            'Citation' ) NOT NULL
+    relationship      VARCHAR(255) NOT NULL
 )
 ENGINE=InnoDB CHARACTER SET UTF8 COLLATE utf8_general_ci;
 
@@ -146,6 +194,7 @@ ALTER TABLE work_work_relationships ADD INDEX toFromIdx (to_id, relationship, fr
 
 ALTER TABLE work_work_relationships ADD CONSTRAINT ww_from_fk FOREIGN KEY (from_id) REFERENCES works(id);
 ALTER TABLE work_work_relationships ADD CONSTRAINT ww_to_fk FOREIGN KEY (to_id) REFERENCES works(id);
+ALTER TABLE work_work_relationships ADD CONSTRAINT ww_ww_type_fk FOREIGN KEY (relationship) REFERENCES work_work_relationship_types(relationship);
 
 /*
  * EMAIL ARCHIVE
@@ -161,7 +210,7 @@ CREATE TABLE mail_folders (
     path                TEXT NOT NULL
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-ALTER TABLE mail_folders ADD UNIQUE INDEX PATH_IDX (path(255), parent_id);
+ALTER TABLE mail_folders ADD UNIQUE INDEX MF_PATH_IDX (path(255), parent_id);
 
 INSERT INTO mail_folders
     (created_at, updated_at, parent_id, name, path)
@@ -223,3 +272,46 @@ CREATE TABLE addressings (
 
 ALTER TABLE addressings ADD CONSTRAINT a_p_fk FOREIGN KEY (part_id) REFERENCES parts(id);
 
+/*
+ * CONTENT
+ */
+
+CREATE TABLE content_folders (
+    id                  INTEGER PRIMARY KEY AUTO_INCREMENT,
+    created_at          DATETIME,
+    updated_at          DATETIME,
+    lock_version        INTEGER NOT NULL DEFAULT 0,
+    parent_id           INTEGER NOT NULL,
+    name                VARCHAR(255) NOT NULL,
+    path                TEXT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+ALTER TABLE content_folders ADD UNIQUE INDEX CF_PATH_IDX (path(255), parent_id);
+
+INSERT INTO content_folders
+    (created_at, updated_at, parent_id, name, path)
+VALUES
+    (NOW(), NOW(), 0, "/", "/");
+
+UPDATE content_folders SET id = 0;
+
+ALTER TABLE content_folders ADD CONSTRAINT cf_cf_fk FOREIGN KEY (parent_id) REFERENCES content_folders(id);
+
+CREATE TABLE content_items (
+    id                  INTEGER PRIMARY KEY AUTO_INCREMENT,
+    created_at          DATETIME,
+    updated_at          DATETIME,
+    lock_version        INTEGER NOT NULL DEFAULT 0,
+    parent_id           INTEGER NOT NULL,
+    name                VARCHAR(255) NOT NULL,
+    path                TEXT NOT NULL,
+    type                ENUM('binary_content_item',
+                              'text_content_item') NOT NULL,
+    text_content        MEDIUMTEXT,
+    binary_content      MEDIUMBLOB,
+    size                INTEGER,
+    mime_type           VARCHAR(255) NOT NULL,
+    copyright           TEXT
+) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+ALTER TABLE content_items ADD CONSTRAINT ci_cf_fk FOREIGN KEY (parent_id) REFERENCES content_folders(id);
--- sql/mysql/dropSelfReferentialConstraints.sql
+++ sql/mysql/dropSelfReferentialConstraints.sql
@@ -1,3 +1,4 @@
 ALTER TABLE mail_folders DROP FOREIGN KEY mf_mf_fk;
 ALTER TABLE parts DROP FOREIGN KEY p_p_fk;
 ALTER TABLE parts DROP FOREIGN KEY p_mf_fk;
+ALTER TABLE content_folders DROP FOREIGN KEY cf_cf_fk;
