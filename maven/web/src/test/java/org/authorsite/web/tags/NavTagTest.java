31ed49d4f081ce48f6ba4e0eebcc0280d5290268
jejking <jejking@ba34ae75-9d15-0410-b136-fd2a35c96f86>
2007-08-05 21:22:34 +0000

    start of test for nav tag

---
+++ maven/web/src/test/java/org/authorsite/web/tags/NavTagTest.java
@@ -0,0 +1,58 @@
+package org.authorsite.web.tags;
+
+import java.util.Locale;
+
+import org.authorsite.web.Jee5MockServletContext;
+import org.authorsite.web.MockJspWriter;
+import org.authorsite.web.WriterCapableMockPageContext;
+import org.authorsite.web.nav.NavNode;
+import org.authorsite.web.nav.XmlNavNodeFactory;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockPageContext;
+import org.springframework.mock.web.MockServletContext;
+
+import junit.framework.TestCase;
+
+public class NavTagTest extends TestCase {
+
+    private NavTag navTag;
+    private MockJspWriter mockJspWriter;
+    private MockHttpServletRequest request; 
+    private WriterCapableMockPageContext pageContext;
+    private Jee5MockServletContext servletContext;
+    
+    
+    public NavTagTest(String arg0) {
+	super(arg0);
+    }
+
+    protected void setUp() throws Exception {
+	super.setUp();
+	
+	
+	this.request = new MockHttpServletRequest();
+	this.request.addPreferredLocale(Locale.ENGLISH);
+	this.request.setRequestURI("/myWebContext/people/individuals/123");
+	this.request.setContextPath("/myWebContext");
+	
+	this.servletContext = new Jee5MockServletContext();
+	this.servletContext.setContextPath("/myWebContext");
+	
+	this.pageContext = new WriterCapableMockPageContext(this.servletContext, request);
+	
+	this.mockJspWriter = new MockJspWriter(1, false);
+	this.pageContext.setMockJspWriter(this.mockJspWriter);
+	
+	this.navTag = new NavTag();
+	this.navTag.setPageContext(this.pageContext);
+	
+	XmlNavNodeFactory.buildInstance("/testNav3.xml");
+    }
+
+    public void testDoStartTag() throws Exception {
+	this.navTag.doStartTag();
+	System.out.println(mockJspWriter.getPrinted());
+	
+    }
+
+}
